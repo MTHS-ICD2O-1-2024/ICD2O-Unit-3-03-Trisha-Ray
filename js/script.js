e89cbdac1baaed81804e55b657ebe86e45200b13
default <trisha.ray@stu.ocsb.ca>
2025-04-02 16:57:54 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolumeOfPyramid () {
+  // input
+  const length = parseFloat(document.getElementById('length').value);
+  const width = parseFloat(document.getElementById('width').value);
+  const height = parseFloat(document.getElementById('height').value);
+
+  // process
+  const volumeOfPyramid =  (length * width * height) / 3;
+
+  // output
+  document.getElementById('area').innerHTML = 'Volume is: ' + volumeOfPyramid + ' mm³'
+}
+
